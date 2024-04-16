6cff7f7e5a4cfd2de11699c46bb9fc6fa0edba8b
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2024-04-15 21:49:49 -0700

    fix: conforming to a protocol does not imply in conforming to NSObject (#46)

--- types/objc/type_encoding.go
+++ types/objc/type_encoding.go
@@ -181,7 +181,7 @@ func getPropertyType(attrs string) (typ string) {
 		if strings.HasPrefix(attr, "@\"") {
 			typ = strings.Trim(attr, "@\"")
 			if strings.HasPrefix(typ, "<") {
-				typ = "NSObject" + strings.ReplaceAll(typ, "><", ", ")
+				typ = "id " + strings.ReplaceAll(typ, "><", ", ")
 			}
 			typ += " *"
 		} else {
@@ -249,7 +249,7 @@ func getIVarType(ivType string) string {
 	if strings.HasPrefix(ivType, "@\"") && len(ivType) > 1 {
 		ivType = strings.Trim(ivType, "@\"")
 		if strings.HasPrefix(ivType, "<") {
-			ivType = "NSObject" + strings.ReplaceAll(ivType, "><", ", ")
+			ivType = "id " + strings.ReplaceAll(ivType, "><", ", ")
 		}
 		return ivType + " *"
 	}
